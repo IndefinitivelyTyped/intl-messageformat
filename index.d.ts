dd6ee2b27576c1f1d498c7807ccf2f7ef20778a1
Max Battcher <me@worldmaker.net>
2016-06-14 16:33:08 -0400

    Merge class export and declaration

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,4 @@
-declare class IntlMessageFormat {
+export declare default class IntlMessageFormat {
     constructor(message: string, locales: string | string[], formats?: any)
     format(context?: any): string
 }
-
-export default IntlMessageFormat
